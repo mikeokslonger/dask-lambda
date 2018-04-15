a0d04a34a5be796a9cefdb372bfc3767ab9872eb
mikeokslonger <hattrickeric@gmail.com>
2018-04-15 19:26:37 +0100

    Connects to scheduler, port forwards and calculates tasks (does not yet return results)

--- aws_lambda/run.py
+++ aws_lambda/run.py
@@ -1,21 +1,24 @@
 from distributed import Worker
 from distributed.cli import dask_worker
 from distributed.worker import LambdaWorker
+from distributed.comm import get_local_address_for
 from tornado.ioloop import IOLoop
 from tornado import gen
+import multiprocessing
 import threading
 import paramiko
 import socket
 import select
 import boto3
+import time
 import os
 
 SSH_PORT = 22
 s3 = boto3.resource('s3')
 
 
-def handler(chan, host, port):
-    print('Running handler')
+def handler(sock, chan, host, port):
+    print('running handler')
     sock = socket.socket()
     try:
         sock.connect((host, port))
@@ -42,34 +45,78 @@ def handler(chan, host, port):
     print('Tunnel closed from %r' % (chan.origin_addr,))
 
 
-def reverse_forward_tunnel(server_port, remote_host, remote_port, transport):
+def reverse_forward_tunnel(sock, server_port, remote_host, remote_port, transport):
+    print('running reverse_forward_tunnel')
     transport.request_port_forward('', server_port)
+    print('Starting loop')
     while True:
+        print('Running loop')
         chan = transport.accept(1000)
+        print('Accepted chan')
         if chan is None:
             continue
-        handler(chan, remote_host, remote_port)
-        #thr = threading.Thread(target=handler, args=(chan, remote_host, remote_port))
-        #thr.setDaemon(True)
-        #thr.start()
+        handler(sock, chan, remote_host, remote_port)
 
 
-def forward(user, ssh_key, remote_url, remote_port, local_port):
+def forward(sock, user, ssh_key, remote_url, remote_port, local_port):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
     client.connect(remote_url, 22, username=user, key_filename=ssh_key)
-    t = threading.Thread(target=reverse_forward_tunnel, args=(remote_port, 'localhost', local_port, client.get_transport()))
-    t.setDaemon(True)
-    t.start()
-    return t
+
+    my_local_ip = get_local_address_for('{}:{}'.format(remote_url, remote_port))[6:]
+
+    #t = threading.Thread(target=reverse_forward_tunnel, args=(sock, remote_port, 'localhost', local_port, client.get_transport()))
+    #t.setDaemon(True)
+    #t.start()
+    #time.sleep(60)
+    #reverse_forward_tunnel(sock, remote_port, 'localhost', local_port, client.get_transport())
+    reverse_forward_tunnel(sock, remote_port, my_local_ip, local_port, client.get_transport())
+
+    #return t
+
+
+def run_worker(scheduler_addr):
+    print('Starting dask worker')
+    loop = IOLoop.current()
+    w = LambdaWorker('tcp://' + scheduler_addr, loop=loop, local_dir='/tmp/', contact_address=None, name='', reconnect=False)
+
+    @gen.coroutine
+    def run():
+        written = False
+        yield w._start(None)
+        while w.status != 'closed':
+            if not written:
+                with open('/tmp/port', 'w') as f:
+                    f.write(str(w.port))
+                written = True
+            yield gen.sleep(0.2)
+
+
+    try:
+        loop.run_sync(run)
+    except (KeyboardInterrupt, TimeoutError):
+        pass
+    finally:
+        print("End worker")
+
+
+def run_forward(user, new_cert_location, scheduler_ip):
+    while not os.path.exists('/tmp/port'):
+        time.sleep(0.2)
+
+    port = int(open('/tmp/port', 'r').read())
+    forward(None, user, new_cert_location, scheduler_ip, port, port)
 
 
 def run(event, context):
     print('Starting')
+    if os.path.exists('/tmp/port'):
+        os.remove('/tmp/port')
+
     scheduler_ip = event['ip']
     scheduler_port = event['port']
-    worker_remote_port = event['worker_remote_port']
+    #worker_remote_port = event['worker_remote_port']
 
     user = event['user']
     ssh_key = event['ssh_key']
@@ -85,47 +132,23 @@ def run(event, context):
 
     scheduler_addr = '{}:{}'.format(scheduler_ip, scheduler_port)
     print('Starting worker connecting to scheduler at: {}'.format(scheduler_addr))
+    p_worker = multiprocessing.Process(target=run_worker, args=(scheduler_addr,))
+    p_worker.start()
 
-    # Registering tcp://169...., tcp://172...., tcp://172....
-    #dask_worker.main.main(['tcp://' + scheduler_addr, '--no-nanny',
-    #                       '--local-directory', '/tmp/',
-    #                       '--listen-address', 'tcp://0.0.0.0',
-    #                       '--contact-address', '',
-    #                       ])
-
-    print('Starting dask worker')
-    loop = IOLoop.current()
-    w = LambdaWorker('tcp://' + scheduler_addr, local_dir='/tmp/', loop=loop, contact_address=None, name='')
-
-    def run_worker(w):
-        @gen.coroutine
-        def run():
-            yield w._start(None)
-            while w.status != 'closed':
-                yield gen.sleep(0.2)
-
-        w._start(None)
-
-        try:
-            loop.run_sync(run)
-        except (KeyboardInterrupt, TimeoutError):
-            pass
-        finally:
-            print("End worker")
-
-    import threading
-    t = threading.Thread(target=run_worker, args=(w,))
-    t.setDaemon(True)
-    t.start()
-
-    import time
     time.sleep(1)
+    p_forwarder = multiprocessing.Process(target=run_forward, args=(user, new_cert_location, scheduler_ip))
+    p_forwarder.start()
 
-
-    print('Starting forwarding')
-    t = forward(user, new_cert_location, scheduler_ip, remote_port=worker_remote_port, local_port=w.port)
-
-    import time
-    time.sleep(10)
+    time.sleep(12)
 
     print('Returning')
+
+
+if __name__ == '__main__':
+    event = {
+        "ip": "34.245.189.22",
+        "port": 8786,
+        "user": "ec2-user",
+        "ssh_key": "mikeokslonger-dask/ec2.pem"
+    }
+    run(event, None)
